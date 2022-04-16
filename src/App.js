623500ea9c022295041d4f54123ed75be529afc5
Sajalhowlader <sajolhowlader08@gmail.com>
2022-04-16 21:50:29 +0600

    add navber from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Sheared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <Routes>
+        <Route path='home' element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/Pages/Sheared/Header/Header.js
+++ src/Pages/Sheared/Header/Header.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import logo from '../../../images/header-logo.png'
 const Header = () => {
     return (
-        <nav>
-            <div className='header-container'>
-                <div className="logo">
-
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container">
+                <div>
+                    <img src={logo} alt="" />
                 </div>
-                <div className="menu">
-                    <NavLink to='/home'>HOME</NavLink>
-                    <NavLink to='/schedule'>SCHEDULE</NavLink>
-                    <NavLink to='/services'>SERVICES</NavLink>
-                    <NavLink to='/about'>ABOUT</NavLink>
-                    <NavLink to='/contacts'>CONTACTS</NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" aria-current="page" to="/home">Home</NavLink>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
                 </div>
             </div>
         </nav>
