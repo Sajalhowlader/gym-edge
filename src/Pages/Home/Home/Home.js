1cf33ea8e8a93fe5ad0cfdd89ea768fc31e3490b
Sajalhowlader <sajolhowlader08@gmail.com>
2022-04-17 00:05:27 +0600

    add some routes

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
-// import Header from '../../Sheared/Header/Header';
+import Header from '../../Sheared/Header/Header';
+import instructorLogo from '../../../images/trainer.png'
 import './Home.css'
 const Home = () => {
     return (
         <div className='home-container'>
-
+            <Header />
+            <div className="instructor">
+                <div className="instructor-logo">
+                    <img className='img-fluid' src={instructorLogo} alt="" />
+                </div>
+                <div className='instructor-info'>
+                    <h2>Hi</h2>
+                    <h1>I'm <span>Antu Howlader</span></h1>
+                    <p>Body Building and Yoga Instructor </p>
+                    <button>About me</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Sheared/Header/Header.js
+++ src/Pages/Sheared/Header/Header.js
@@ -3,19 +3,21 @@ import { NavLink } from 'react-router-dom';
 import logo from '../../../images/header-logo.png'
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light ">
             <div className="container">
                 <div>
-                    <img src={logo} alt="" />
+                    <img className='img-fluid' src={logo} alt="" />
                 </div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <NavLink className="nav-link active" aria-current="page" to="/home">Home</NavLink>
-                        </li>
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <NavLink className="nav-link active" aria-current="page" to="/home">HOME</NavLink>
+                        <NavLink className="nav-link active" aria-current="page" to="/Schedule">SCHEDULE</NavLink>
+                        <NavLink className="nav-link active" aria-current="page" to="/services">SERVICES</NavLink>
+                        <NavLink className="nav-link active" aria-current="page" to="/about">ABOUT</NavLink>
+                        <NavLink className="nav-link active" aria-current="page" to="/contacts">CONTACTS</NavLink>
                     </ul>
                     <form className="d-flex">
                         <button className="btn btn-outline-success" type="submit">Search</button>
