1d309ab6dcbcc33b3da862f04e2ba0b039c42f61
Sajalhowlader <sajolhowlader08@gmail.com>
2022-04-17 14:21:02 +0600

    add sing up from

--- src/Pages/Submition/SingIn/SingIn.js
+++ src/Pages/Submition/SingIn/SingIn.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import Header from '../../Sheared/Header/Header';
+import bgImg from '../../../images/loginImg/bg.svg'
+import avatar from '../../../images/loginImg/avatar.svg'
 import './SingIn.css'
 const SingIn = () => {
     return (
-        <div className='login-container'>
+        <div >
             <Header color="black" />
-            <form>
+            <div className='login-container'>
+                <form>
+                    <div className='login-info-container'>
+                        <div className='login-logo'>
+                            <img src={bgImg} alt="" />
+                        </div>
+                        <div className="login-info">
+                            <div className='info'>
+                                <img src={avatar} alt="" />
+                                <h2 className=''>Welcome</h2>
+                                <input className='sing-in-input' type="email" name="" id="" placeholder='Your Email' />
+                                <input className='sing-in-input' type="password" name="" id="" placeholder='Your password' />
+                                <p>New Here? <strong className='first-res'>Resister first</strong></p>
+                                <input className='sing-up-btn' type="submit" value="SING UP" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
 
-            </form>
+            </div>
         </div>
     );
 };
